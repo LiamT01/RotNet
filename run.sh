e32073d979b7a3e79fb2b6d47a2f7b796bc01977
Hongwei Tu <tutu2019@sjtu.edu.cn>
2022-08-10 12:48:19 +0800

    Merge single-card and distributed training into train.py

--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
+#CUDA_VISIBLE_DEVICES=0,1,2,4,6,7,8,9 python train.py \
+#  --distributed --num_nodes 1 --num_gpus_per_node 8 --node_rank 0 \
+#  --dataset_name qm7 --cutoff 6 --lr 0.001 --batch_size 32 --epochs 10000 \
+#  --num_layers 5 --gaussian_num_steps 50 --x_size 92 --hidden_size 512 \
+#  --use_invariance
+
 CUDA_VISIBLE_DEVICES=5 python train.py \
   --dataset_name qm7 --cutoff 6 --lr 0.001 --batch_size 256 --epochs 10000 \
   --num_layers 5 --gaussian_num_steps 50 --x_size 92 --hidden_size 512 \
--- train.py
+++ train.py
@@ -1,12 +1,14 @@
 import argparse
+import os
 import os.path as osp
 import pdb
 import random
 from datetime import datetime
-from sys import exit
 
 import numpy as np
 import torch
+import torch.distributed as dist
+import torch.multiprocessing as mp
 from dataset import GraphDataset
 from models import GNN
 from torch_geometric.loader import DataLoader
@@ -14,132 +16,235 @@ from tqdm import tqdm
 from utils import get_logger, reduce_losses
 from weights import weights
 
-output_dir = f'exp/train_{datetime.now():%Y-%m-%d_%H:%M:%S}'
-logger = get_logger(osp.join(output_dir, 'train.log'))
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--use_invariance', action='store_true')
-parser.add_argument('--dataset_name', type=str, required=True)
-parser.add_argument('--cutoff', default=4, type=float)
-parser.add_argument('--lr', default=1e-3, type=float)
-parser.add_argument('--batch_size', '--bs', default=64, type=int)
-parser.add_argument('--seed', default=0, type=int)
-parser.add_argument('--epochs', default=100, type=int)
-parser.add_argument('--num_layers', default=5, type=int)
-parser.add_argument('--gaussian_num_steps', default=50, type=int)
-parser.add_argument('--x_size', default=92, type=int)
-parser.add_argument('--hidden_size', default=512, type=int)
-args = parser.parse_args()
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--distributed', action='store_true')
+    parser.add_argument('--use_invariance', action='store_true')
+    parser.add_argument('--dataset_name', type=str, required=True)
+    parser.add_argument('--cutoff', default=4, type=float)
+    parser.add_argument('--lr', default=1e-3, type=float)
+    parser.add_argument('--batch_size', '--bs', default=64, type=int)
+    parser.add_argument('--seed', default=0, type=int)
+    parser.add_argument('--epochs', default=100, type=int)
+    parser.add_argument('--num_layers', default=5, type=int)
+    parser.add_argument('--gaussian_num_steps', default=50, type=int)
+    parser.add_argument('--x_size', default=92, type=int)
+    parser.add_argument('--hidden_size', default=512, type=int)
 
-random.seed(args.seed)
-np.random.seed(args.seed)
-torch.manual_seed(args.seed)
-torch.cuda.manual_seed_all(args.seed)
-torch.backends.cudnn.deterministic = True
-torch.backends.cudnn.benchmark = False
+    #########################################################
+    parser.add_argument('--num_nodes', default=1, type=int, metavar='N')
+    parser.add_argument('--num_gpus_per_node', default=1, type=int, help='Number of gpus per node.')
+    parser.add_argument('--node_rank', default=0, type=int, help='Ranking within the nodes.')
+    #########################################################
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    args = parser.parse_args()
 
-train_set = GraphDataset(root='data',
-                         dataset_name=args.dataset_name,
-                         ele_label='data/ele.json',
-                         atom_embed='data/atom_init_embedding.json',
-                         radii='data/radii.json',
-                         split='train',
-                         cutoff=args.cutoff,
-                         use_invariance=args.use_invariance)
-val_set = GraphDataset(root='data',
-                       dataset_name=args.dataset_name,
-                       ele_label='data/ele.json',
-                       atom_embed='data/atom_init_embedding.json',
-                       radii='data/radii.json',
-                       split='val',
-                       cutoff=args.cutoff,
-                       use_invariance=args.use_invariance)
+    if args.distributed:
+        ############################################################
+        args.world_size = args.num_gpus_per_node * args.num_nodes  #
+        os.environ['MASTER_ADDR'] = '0.0.0.0'                      #
+        os.environ['MASTER_PORT'] = '7777'                         #
+        mp.spawn(train, nprocs=args.num_gpus_per_node, args=(args,))            #
+        ############################################################
 
-train_loader = DataLoader(train_set, batch_size=args.batch_size, shuffle=True)
-val_loader = DataLoader(val_set, batch_size=args.batch_size)
+    train(0, args)
 
-model = GNN(num_layers=args.num_layers, x_size=args.x_size, hidden_size=args.hidden_size,
-            cutoff=args.cutoff, gaussian_num_steps=args.gaussian_num_steps, targets=train_set.metadata['targets'])
-model = model.float().to(device)
 
-Loss = torch.nn.L1Loss()
-optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
+def train(gpu, args):
+    rank = None
+    if args.distributed:
+        ############################################################
+        rank = args.node_rank * args.num_gpus_per_node + gpu
+        dist.init_process_group(
+            backend='nccl',
+            init_method='env://',
+            world_size=args.world_size,
+            rank=rank
+        )
+        ############################################################
 
-logger.info(args)
-logger.info(args.dataset_name)
-logger.info(model)
-logger.info(f'Average edges per node: {train_set.metadata["averageEdgesPerNode"]}')
+    random.seed(args.seed)
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    torch.cuda.manual_seed_all(args.seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
 
-best_val_loss = float('inf')
+    if not args.distributed or rank == 0:
+        output_dir = f'exp/train_{datetime.now():%Y-%m-%d_%H:%M:%S}'
+        logger = get_logger(osp.join(output_dir, 'train.log'))
 
-for epoch in tqdm(range(1, args.epochs + 1)):
-    model.train()
-    sum_train_losses = 0
-    train_results_per_epoch = []
-    for idx, batch in enumerate(train_loader):
-        batch = batch.to(device)
-        optimizer.zero_grad()
-        total_loss_per_batch, train_results_per_batch = model(batch, Loss)
-        total_loss_per_batch.backward()
-        optimizer.step()
+    torch.cuda.set_device(gpu)
 
-        sum_train_losses += total_loss_per_batch.item()
-        train_results_per_epoch.append(train_results_per_batch)
+    train_set = GraphDataset(root='data',
+                             dataset_name=args.dataset_name,
+                             ele_label='data/ele.json',
+                             atom_embed='data/atom_init_embedding.json',
+                             radii='data/radii.json',
+                             split='train',
+                             cutoff=args.cutoff,
+                             use_invariance=args.use_invariance)
 
-        if idx % 20 == 0:
-            print(f'Batch {idx + 1}: training loss = {total_loss_per_batch.item()}')
+    val_set = GraphDataset(root='data',
+                           dataset_name=args.dataset_name,
+                           ele_label='data/ele.json',
+                           atom_embed='data/atom_init_embedding.json',
+                           radii='data/radii.json',
+                           split='val',
+                           cutoff=args.cutoff,
+                           use_invariance=args.use_invariance)
+    if args.distributed:
+        ################################################################
+        train_sampler = torch.utils.data.distributed.DistributedSampler(
+            train_set,
+            num_replicas=args.world_size,
+            rank=rank
+        )
 
-    model.eval()
-    sum_val_losses = 0
-    val_results_per_epoch = []
-    with torch.no_grad():
-        for batch in val_loader:
-            batch = batch.to(device)
-            total_loss_per_batch, val_results_per_batch = model(batch, Loss)
-            sum_val_losses += total_loss_per_batch.item()
-            val_results_per_epoch.append(val_results_per_batch)
+        val_sampler = torch.utils.data.distributed.DistributedSampler(
+            val_set,
+            num_replicas=args.world_size,
+            rank=rank
+        )
+        ################################################################
 
-    train_reduced_losses = reduce_losses(train_results_per_epoch)
-    val_reduced_losses = reduce_losses(val_results_per_epoch)
+    if args.distributed:
+        train_loader = DataLoader(
+            train_set,
+            batch_size=args.batch_size,
+            ##############################
+            shuffle=False,
+            num_workers=0,
+            pin_memory=False,
+            sampler=train_sampler,
+            ##############################
+        )
 
-    t = [train_reduced_losses['count'], val_reduced_losses['count']] + \
-        [sum_train_losses, sum_val_losses] + \
-        [*train_reduced_losses['sum loss'].values()] + \
-        [*train_reduced_losses['sum relative loss'].values()] + \
-        [*val_reduced_losses['sum loss'].values()] + \
-        [*val_reduced_losses['sum relative loss'].values()]
-    t = torch.tensor(t, dtype=torch.float, device='cuda')
+        val_loader = DataLoader(
+            val_set,
+            batch_size=args.batch_size,
+            ##############################
+            shuffle=False,
+            num_workers=0,
+            pin_memory=False,
+            sampler=val_sampler,
+            ##############################
+        )
+    else:
+        train_loader = DataLoader(train_set, batch_size=args.batch_size, shuffle=True)
+        val_loader = DataLoader(val_set, batch_size=args.batch_size)
 
-    num_losses = len(train_set.metadata['targets'])
-    t[2] = t[2] / t[0]
-    t[3] = t[3] / t[1]
-    t[4: 2 * num_losses + 4] = t[4: 2 * num_losses + 4] / t[0]
-    t[2 * num_losses + 4:] = t[2 * num_losses + 4:] / t[1]
-    t = t[2:].tolist()
+    model = GNN(num_layers=args.num_layers, x_size=args.x_size, hidden_size=args.hidden_size,
+                cutoff=args.cutoff, gaussian_num_steps=args.gaussian_num_steps, targets=train_set.metadata['targets'])
+    model = model.float().cuda(gpu)
 
-    logger.info(f'Epoch {epoch}, Total: train_loss={t[0]:.8f}, val_loss={t[1]:.8f}')
+    if args.distributed:
+        ###############################################################
+        model = torch.nn.SyncBatchNorm.convert_sync_batchnorm(model)
+        model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[gpu])
+        ###############################################################
 
-    for name, \
-        train_loss, \
-        train_relative_loss, \
-        val_loss, \
-        val_relative_loss in zip([target['name'] for target in train_set.metadata['targets']],
-                                 t[2: num_losses + 2],
-                                 t[num_losses + 2: 2 * num_losses + 2],
-                                 t[2 * num_losses + 2: 3 * num_losses + 2],
-                                 t[3 * num_losses + 2:]):
-        logger.info(f'\t{name}, '
-                    f'Train: loss={train_loss:.8f}, relative_loss={train_relative_loss:.8f}, '
-                    f'Val: loss={val_loss:.8f}, relative_loss={val_relative_loss:.8f}')
+    loss_fn = torch.nn.L1Loss().cuda(gpu)
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
 
-    if t[1] < best_val_loss:
-        logger.info(f'Best val_loss={t[1]} so far was found! Model weights were saved.')
-        if epoch < 400:
-            torch.save(model.state_dict(), osp.join(output_dir, 'best_weights_early_epoch.pth'))
-        else:
-            num_digits = int(np.ceil(np.log(args.epochs) / np.log(10) + 1))
-            torch.save(model.state_dict(), osp.join(output_dir, f'best_weights_epoch_{epoch:0{num_digits}d}.pth'))
+    if not args.distributed or rank == 0:
+        logger.info(args)
+        logger.info(args.dataset_name)
+        logger.info(model)
+        logger.info(f'Average edges per node: {train_set.metadata["averageEdgesPerNode"]}')
 
-        best_val_loss = t[1]
+    best_val_loss = float('inf')
+
+    for epoch in tqdm(range(1, args.epochs + 1)):
+        if args.distributed:
+            train_loader.sampler.set_epoch(epoch)
+
+        model.train()
+        sum_train_losses = 0
+        train_results_per_epoch = []
+
+        for idx, batch in enumerate(train_loader):
+            batch = batch.cuda(non_blocking=True)
+            optimizer.zero_grad()
+            total_loss_per_batch, train_results_per_batch = model(batch, loss_fn)
+            total_loss_per_batch.backward()
+            optimizer.step()
+
+            sum_train_losses += total_loss_per_batch.item()
+            train_results_per_epoch.append(train_results_per_batch)
+
+            if idx % 20 == 0:
+                print(f'Batch {idx + 1}: training loss = {total_loss_per_batch.item()}')
+
+        model.eval()
+        sum_val_losses = 0
+        val_results_per_epoch = []
+        with torch.no_grad():
+            for batch in val_loader:
+                batch = batch.cuda(non_blocking=True)
+                total_loss_per_batch, val_results_per_batch = model(batch, loss_fn)
+                sum_val_losses += total_loss_per_batch.item()
+                val_results_per_epoch.append(val_results_per_batch)
+
+        train_reduced_losses = reduce_losses(train_results_per_epoch)
+        val_reduced_losses = reduce_losses(val_results_per_epoch)
+
+        t = [train_reduced_losses['count'], val_reduced_losses['count']] + \
+            [sum_train_losses, sum_val_losses] + \
+            [*train_reduced_losses['sum loss'].values()] + \
+            [*train_reduced_losses['sum relative loss'].values()] + \
+            [*val_reduced_losses['sum loss'].values()] + \
+            [*val_reduced_losses['sum relative loss'].values()]
+
+        t = torch.tensor(t, dtype=torch.float, device='cuda')
+
+        if args.distributed:
+            dist.barrier()
+            dist.all_reduce(t, op=dist.ReduceOp.SUM)
+
+        num_losses = len(train_set.metadata['targets'])
+        t[2] = t[2] / t[0]
+        t[3] = t[3] / t[1]
+        t[4: 2 * num_losses + 4] = t[4: 2 * num_losses + 4] / t[0]
+        t[2 * num_losses + 4:] = t[2 * num_losses + 4:] / t[1]
+        t = t[2:].tolist()
+
+        if not args.distributed or rank == 0:
+            logger.info(f'Epoch {epoch}, Total: train_loss={t[0]:.8f}, val_loss={t[1]:.8f}')
+
+            for name, \
+                train_loss, \
+                train_relative_loss, \
+                val_loss, \
+                val_relative_loss in zip([target['name'] for target in train_set.metadata['targets']],
+                                         t[2: num_losses + 2],
+                                         t[num_losses + 2: 2 * num_losses + 2],
+                                         t[2 * num_losses + 2: 3 * num_losses + 2],
+                                         t[3 * num_losses + 2:]):
+
+                logger.info(f'\t{name}, '
+                            f'Train: loss={train_loss:.8f}, relative_loss={train_relative_loss:.8f}, '
+                            f'Val: loss={val_loss:.8f}, relative_loss={val_relative_loss:.8f}')
+
+            if t[1] < best_val_loss:
+                logger.info(f'\t\tBest val_loss={t[1]} so far was found! Model weights were saved.')
+                if epoch < 400:
+                    if args.distributed:
+                        torch.save(model.module.state_dict(), osp.join(output_dir, 'best_weights_early_epoch.pth'))
+                    else:
+                        torch.save(model.state_dict(), osp.join(output_dir, 'best_weights_early_epoch.pth'))
+                else:
+                    num_digits = int(np.ceil(np.log(args.epochs) / np.log(10) + 1))
+                    if args.distributed:
+                        torch.save(model.module.state_dict(),
+                                   osp.join(output_dir, f'best_weights_epoch_{epoch:0{num_digits}d}.pth'))
+                    else:
+                        torch.save(model.state_dict(),
+                                   osp.join(output_dir, f'best_weights_epoch_{epoch:0{num_digits}d}.pth'))
+
+                best_val_loss = t[1]
+
+
+if __name__ == '__main__':
+    main()
